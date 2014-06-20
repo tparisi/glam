fd5ded42e3e16a9f3b9315ebba2c9685d66979e1
Tony Parisi <tparisi@gmail.com>
2014-06-20 11:17:18 -0700

    Fixes from text regression

--- src/visual.js
+++ src/visual.js
@@ -26,12 +26,15 @@ glam.Visual.createVisual = function(obj, cls, docelt, material, param) {
 	var visual = cls.createVisual(docelt, material, param);	
 	if (visual) {
 		obj.addComponent(visual);
-		glam.Visual.addProperties(docelt, visual);
+		glam.Visual.addProperties(docelt, obj);
 	}
 }
 
-glam.Visual.addProperties = function(docelt, visual) {
+glam.Visual.addProperties = function(docelt, obj) {
 
+	var visuals = obj.getComponents(Vizi.Visual);
+	var visual = visuals[0];
+	
 	if (visual) {
 		// Is this the API?	
 		docelt.geometry = visual.geometry;
