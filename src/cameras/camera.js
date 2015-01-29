c3147923a84226d21c5116a19a478df71243d083
rangermauve <RangerMauve@hotmail.com>
2015-01-28 21:41:28 -0500

    Updated src/camera

--- src/cameras/camera.js
+++ src/cameras/camera.js
@@ -1,59 +1,64 @@
-goog.provide('glam.Camera');
-goog.require('glam.SceneComponent');
+// goog.provide('Camera');
 
-glam.Camera = function(param)
+module.exports = Camera;
+
+var SceneComponent = require("../scene/sceneComponent");
+var CameraManager = require("./cameraManager");
+var util = require("util");
+
+util.inherits(Camera, SceneComponent);
+
+function Camera(param)
 {
 	param = param || {};
-	
-	glam.SceneComponent.call(this, param);
 
-    // Accessors
-    Object.defineProperties(this, {
-        active: {
-	        get: function() {
-	            return this._active;
-	        },
-	        set: function(v) {
-	        	this._active = v;
-	        	// N.B.: trying this out for now... TP
-	        	if (/*this._realized && */ this._active)
-	        	{
-	        		glam.CameraManager.setActiveCamera(this);
-	        	}
-	        }
-    	},    	
+	SceneComponent.call(this, param);
 
-    });
-	
+	// Accessors
+	Object.defineProperties(this, {
+			active: {
+				get: function() {
+						return this._active;
+				},
+				set: function(v) {
+					this._active = v;
+					// N.B.: trying this out for now... TP
+					if (/*this._realized && */ this._active)
+					{
+						CameraManager.setActiveCamera(this);
+					}
+				}
+		},
+
+	});
+
 	this._active = param.active || false;
-	var position = param.position || glam.Camera.DEFAULT_POSITION;
-    //this.position.copy(position);	
+	var position = param.position || Camera.DEFAULT_POSITION;
+	//this.position.copy(position);
 }
 
-goog.inherits(glam.Camera, glam.SceneComponent);
+Camera.prototype._componentProperty = "camera";
+Camera.prototype._componentPropertyType = "Camera";
 
-glam.Camera.prototype._componentProperty = "camera";
-glam.Camera.prototype._componentPropertyType = "Camera";
-
-glam.Camera.prototype.realize = function() 
+Camera.prototype.realize = function()
 {
-	glam.SceneComponent.prototype.realize.call(this);
-	
+	SceneComponent.prototype.realize.call(this);
+
 	this.addToScene();
-	
-	glam.CameraManager.addCamera(this);
-	
-	if (this._active && !glam.CameraManager.activeCamera)
+
+	CameraManager.addCamera(this);
+
+	if (this._active && !CameraManager.activeCamera)
 	{
-		glam.CameraManager.setActiveCamera(this);
+		CameraManager.setActiveCamera(this);
 	}
 }
 
-glam.Camera.prototype.lookAt = function(v) 
+Camera.prototype.lookAt = function(v)
 {
 	this.object.lookAt(v);
 }
 
-glam.Camera.DEFAULT_POSITION = new THREE.Vector3(0, 0, 0);
-glam.Camera.DEFAULT_NEAR = 1;
-glam.Camera.DEFAULT_FAR = 10000;
+Camera.DEFAULT_POSITION = new THREE.Vector3(0, 0, 0);
+Camera.DEFAULT_NEAR = 1;
+Camera.DEFAULT_FAR = 10000;
--- src/cameras/perspectiveCamera.js
+++ src/cameras/perspectiveCamera.js
@@ -1,76 +1,82 @@
-goog.provide('glam.PerspectiveCamera');
-goog.require('glam.Camera');
+goog.provide('PerspectiveCamera');
+goog.require('Camera');
 
-glam.PerspectiveCamera = function(param) {
+module.exports = PerspectiveCamera
+
+var Camera = require("./camera");
+var Graphics = require("../graphics/graphics");
+var util = require("util");
+
+util.inherits(PerspectiveCamera, Camera);
+
+function PerspectiveCamera(param) {
 	param = param || {};
-	
+
 	if (param.object) {
 		this.object = param.object;
 	}
-	else {		
+	else {
 		var fov = param.fov || 45;
-		var near = param.near || glam.Camera.DEFAULT_NEAR;
-		var far = param.far || glam.Camera.DEFAULT_FAR;
-		var container = glam.Graphics.instance.container;
+		var near = param.near || Camera.DEFAULT_NEAR;
+		var far = param.far || Camera.DEFAULT_FAR;
+		var container = Graphics.instance.container;
 		var aspect = param.aspect || (container.offsetWidth / container.offsetHeight);
 		this.updateProjection = false;
-		
+
 		this.object = new THREE.PerspectiveCamera( fov, aspect, near, far );
 	}
-	
-    // Create accessors for all properties... just pass-throughs to Three.js
-    Object.defineProperties(this, {
-        fov: {
-	        get: function() {
-	            return this.object.fov;
-	        },
-	        set: function(v) {
-	        	this.object.fov = v;
-	        	this.updateProjection = true;
-	        }
-		},    	
-        aspect: {
-	        get: function() {
-	            return this.object.aspect;
-	        },
-	        set: function(v) {
-	        	this.object.aspect = v;
-	        	this.updateProjection = true;
-	        }
-    	},    	
-        near: {
-	        get: function() {
-	            return this.object.near;
-	        },
-	        set: function(v) {
-	        	this.object.near = v;
-	        	this.updateProjection = true;
-	        }
-    	},    	
-        far: {
-	        get: function() {
-	            return this.object.far;
-	        },
-	        set: function(v) {
-	        	this.object.far = v;
-	        	this.updateProjection = true;
-	        }
-    	},    	
 
-    });
+		// Create accessors for all properties... just pass-throughs to Three.js
+		Object.defineProperties(this, {
+				fov: {
+					get: function() {
+							return this.object.fov;
+					},
+					set: function(v) {
+						this.object.fov = v;
+						this.updateProjection = true;
+					}
+		},
+				aspect: {
+					get: function() {
+							return this.object.aspect;
+					},
+					set: function(v) {
+						this.object.aspect = v;
+						this.updateProjection = true;
+					}
+			},
+				near: {
+					get: function() {
+							return this.object.near;
+					},
+					set: function(v) {
+						this.object.near = v;
+						this.updateProjection = true;
+					}
+			},
+				far: {
+					get: function() {
+							return this.object.far;
+					},
+					set: function(v) {
+						this.object.far = v;
+						this.updateProjection = true;
+					}
+			},
 
-	glam.Camera.call(this, param);
-	
-    
+		});
+
+	Camera.call(this, param);
+
+
 }
 
-goog.inherits(glam.PerspectiveCamera, glam.Camera);
-
-glam.PerspectiveCamera.prototype.realize = function()  {
-	glam.Camera.prototype.realize.call(this);	
+PerspectiveCamera.prototype.realize = function()  {
+	Camera.prototype.realize.call(this);
 }
 
-glam.PerspectiveCamera.prototype.update = function()  {
+PerspectiveCamera.prototype.update = function()  {
 	if (this.updateProjection)
 	{
 		this.object.updateProjectionMatrix();
